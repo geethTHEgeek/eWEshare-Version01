d18e86a29f7bfd1bdda2c30c833c8f2ccc501bf9
thathsaranandun <46743657+thathsaranandun@users.noreply.github.com>
2019-01-25 10:35:56 +0530

    Added map page

--- eWE-V1/src/app/app.module.ts
+++ eWE-V1/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { AboutPage } from '../pages/about/about';
 import { ContactPage } from '../pages/contact/contact';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
+import { MapPage } from '../pages/map/map';
+
 
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
@@ -17,6 +19,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    MapPage,
     TabsPage
   ],
   imports: [
@@ -29,6 +32,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
     AboutPage,
     ContactPage,
     HomePage,
+    MapPage,
     TabsPage
   ],
   providers: [
